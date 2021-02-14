d7d9f710713e8455b883dc5870f00a0f99130ae0
ko-shimao <74124713+ko-shimao@users.noreply.github.com>
2021-02-14 14:41:28 +0900

    Update kenpeiritsu.js

--- kenpeiritsu.js
+++ kenpeiritsu.js
@@ -1,11 +1,4 @@
-h1{
-   color:darkgreen;
-   background-color:aquamarine;
-   font-size:32px;
-   padding:16px;
-}
-img{
-    width:900px;
-    height:500px;
-    border:8px solid gray;
-}
+alert('建ぺい率');
+var building = prompt("建物面積の値を入力してください");
+var site = prompt("敷地面積 の値を入力してください");
+alert('建ぺい率は'+ building/site*100 +'％')
